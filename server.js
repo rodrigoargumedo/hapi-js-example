08939f50099437006c083d35e765232b8cd3f3aa
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2016-03-19 02:13:39 +0000

    Use and expose inert for html rendering

--- server.js
+++ server.js
@@ -6,24 +6,17 @@ const Hapi = require("hapi");
 const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000, host: process.env.IP || "http://localhost" });
 
-function firstLetter(string) {
-   return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-server.route({
-   method: 'GET',
-   path: '/',
-   handler: function(request, reply) {
-       reply("Welcome to Hapi");
+server.register(require("inert"), (err) => {
+   if (err) {
+      throw err;
    }
-});
-
-server.route({
-   method: 'GET',
-   path: '/{name}',
-   handler: function(request, reply) {
-      reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-   }
+   server.route({
+      method: 'GET',
+      path: '/',
+      handler: function(request, reply) {
+         reply.file('./public/index.html');
+      }
+   })
 });
 
 server.start((err) => {
@@ -31,7 +24,7 @@ server.start((err) => {
        throw err;
    }
    console.log('Server running at:', server.info.uri);
-   });
+});
 
 server.register({ register: Blipp, options: {} }, function(err) {
 });
